fce367e7d285dc6ee1dda32631b2dccb48ec0de1
MihaiS1988 <117821906+MihaiS1988@users.noreply.github.com>
2023-01-23 12:26:02 +0000

    Added questions for quiz

---
+++ Questions.js
@@ -0,0 +1,79 @@
+// This is for creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+    num: 5, 
+    question: "Arrays in JavaScript can be used to store _____",
+    answer: "All of the above",
+    options: [
+        "numbers and strings",
+        "other arrays",
+        "booleans", 
+        "all of the above"
+    ]
+},
+{
+    num: 6,
+    question: "Where is the correct place to insert JavaScript?",
+    answer: "The <head> section and the <body> section are correct",
+    options: [
+        "The <head> section",
+        "The <head> section and the <body> section are correct ",
+        "The <body> section"
+    ]
+},
+{
+    numb: 7,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+ },   
+];
